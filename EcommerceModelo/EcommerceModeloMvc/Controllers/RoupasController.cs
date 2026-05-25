21bf58188d5875318b400e9bcac1dbc6bd12b03b
Antonio Zago <antoniozagodev@gmail.com>
2026-05-25 10:37:23 -0300

    EM-3.2 - Tela de produtos femininos

---
+++ EcommerceModelo/EcommerceModeloMvc/Controllers/RoupasController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Dtos.HomePage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceModeloMvc.Controllers
+{
+    public class RoupasController : Controller
+    {
+        public IActionResult ListarRoupasFemininas()
+        {
+            var produtosMaisVendidosDtos = new List<ProdutoDto>() {
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+                new ProdutoDto {Nome = "Camisa2", FotoPrincipalBase64 = "/images/modelo2.jpg" },
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+                new ProdutoDto {Nome = "Camisa1", FotoPrincipalBase64 = "/images/modelo.jfif" },
+            };
+
+            return View(produtosMaisVendidosDtos);
+        }
+    }
+}
---
+++ EcommerceModelo/EcommerceModeloMvc/wwwroot/js/filtrosRoupasFemininas.js
@@ -0,0 +1,9 @@
+﻿const rangeInput = document.getElementById('faixa-preco');
+const rangeOutput = document.getElementById('faixa-preco-value');
+
+// Set initial value
+rangeOutput.textContent = rangeInput.value;
+
+rangeInput.addEventListener('input', function () {
+    rangeOutput.textContent = this.value + "R$";
+});
